b4599d079f13140706b3193ad8c5b7a42fa7ada8
githubtest1116 <githubtest1116@gmail.com>
2017-12-11 10:13:14 +0000

    have rankgin

--- app/models/want.rb
+++ app/models/want.rb
@@ -1,5 +1,5 @@
 class Want < Ownership
 	def self.ranking
-		self.group(:item_id).order('count_item.id DESC').limit(10).count(:itme_id)
+		self.group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
 	end
 end
